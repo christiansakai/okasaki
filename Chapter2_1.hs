e1f6754ec5bfdc4b5d9a4ea1960d0c483e5bfde6
Christian Sakai <christianmsakai@gmail.com>
2017-12-27 23:18:00 -0500

    WIP 2_1

--- Chapter2_1.hs
+++ Chapter2_1.hs
@@ -10,7 +10,21 @@ class Stack s where
   cons    :: a -> s a -> s a
   head    :: s a -> a
   tail    :: s a -> s a
+
+  -- Figure 2.5. Executing zs = xs ++ ys
+  -- in a functional setting.
+  -- Notice that the argument lists, xs and ys,
+  -- are unaffected by the operation.
   (++)    :: s a -> s a -> s a
+
+  -- Figure 2.6. Executing ys = update(xs, 2, 7).
+  -- Note the sharing between xs and ys.
+  update  :: s a -> Int -> a -> s a
+
+  -- Exercise 2.1. Write a function suffixes of type
+  -- s a -> s a that takes a list xs and returns a list of
+  -- all the suffixes of xs in decreasing order.
+  suffixes :: s a -> s (s a)
 
 
 -- Figure 2.2. Implementation of stacks
@@ -36,10 +50,21 @@ instance Stack [] where
 
   -- (++) :: [a] -> [a] -> [a]
   [] ++ ys      = ys
-  (x:xs) ++ ys  = x:(xs ++ ys)
+  (x:xs) ++ ys  = x : (xs ++ ys)
+
+  -- update  :: s a -> Int -> a -> s a
+  update [] _ _       = error "SUBSCRIPT" 
+  update (x:xs) 0 y   = y : xs 
+  update (x:xs) pos y = update xs (pos - 1) y
+
+  -- suffixes :: s a -> [s a]
+  suffixes []           = [[]]
+  suffixes stack@(x:xs) = stack : (suffixes xs)
+ 
 
 -- Figure 2.3. Implementation of stacks
 -- using a custom data type
+
 
 data CustomStack a 
   = Nil
@@ -68,3 +93,12 @@ instance Stack CustomStack where
   -- (++) :: CustomStack a -> CustomStack a -> CustomStack a
   Nil ++ ys         = ys
   (Cons x xs) ++ ys = Cons x (xs ++ ys)
+
+  -- update  :: s a -> Int -> a -> s a
+  update Nil _ _            = error "SUBSCRIPT" 
+  update (Cons x xs) 0 y    = Cons y xs 
+  update (Cons x xs) pos y  = update xs (pos - 1) y
+
+  -- suffixes :: s a -> s (s a)
+  suffixes Nil                = Cons Nil Nil
+  suffixes stack@(Cons x xs)  = Cons stack (suffixes xs)
